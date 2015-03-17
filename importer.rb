31042ffeeb282915dd58dd4e7e5a1d794bea5632
mcoenca <m.coenca@gmail.com>
2015-03-17 23:40:08 +0100

    added import csv gem

--- importer.rb
+++ importer.rb
@@ -1,6 +1,8 @@
 # This script will use some gems to import a csv table
 # and others to parse it to retrieve objects that can be imported in a database
 
+require "smarter_csv"
+
 def main
     puts "Hello Ruby ! It's been a long time"
 end
