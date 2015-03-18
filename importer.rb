9d3bbb327b8728f8f72da11b527f3dba27f72341
mcoenca <m.coenca@gmail.com>
2015-03-18 21:07:16 +0100

    we have a Maladie object that gets defined with the imported csv

--- importer.rb
+++ importer.rb
@@ -3,6 +3,24 @@
 
 require "smarter_csv"
 
+# Define the maladie class (structure)
+# (In rails it would be a Model)
+
+class Maladie
+  attr_accessor :nom, :symptomes, :prevalence, :contexte, :antecedents
+  
+  def add_symptom (symptome)
+    # If symptomes is not defined we defined it as an array
+    @symptomes ||= []
+    @symptomes.push(symptome)
+    puts 'adding symptom...'
+  end  
+  
+  def to_s
+    puts "Je suis #{@nom ? @nom  : "sans nom"} et j'ai #{ @symptomes ? @symptomes.size.to_s : 0} symptomes"  
+  end
+end  
+
 def main
 
     puts "Hello Ruby ! It's been a long time"
@@ -15,8 +33,20 @@ def main
 
     puts maladies.size.to_s + " maladies imported"
 
-    maladies do |maladie|
-        puts "hello"
+    maladies.each do |maladie|
+        puts maladie
+        # Parse maladie hash to fill the maladie object well
+        
+        # Create new maladie object
+        mal = Maladie.new
+        mal.nom = maladie['maladie']
+        puts mal
+
+        # find the keys matching "symptome" and pass their values to the add symptom method
+        # 
+        maladie.each_key {|key| mal.add_symptom(maladie[key]) if key.include? "symptome"}
+
+        puts mal
     end
 
 end
